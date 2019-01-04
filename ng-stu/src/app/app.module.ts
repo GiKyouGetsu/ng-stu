b619f9e90d211a08733b305c7b5f02d812e782dd
wei41 <wei41@avaya.com>
2019-01-04 10:06:34 +0800

    animations

--- ng-stu/src/app/app.module.ts
+++ ng-stu/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestFlyInComponent } from './test-fly-in/test-fly-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestFlyInComponent
   ],
   imports: [
     BrowserModule
---
+++ ng-stu/src/app/test-fly-in/test-fly-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test-fly-in',
+  templateUrl: './test-fly-in.component.html',
+  styleUrls: ['./test-fly-in.component.scss']
+})
+export class TestFlyInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
