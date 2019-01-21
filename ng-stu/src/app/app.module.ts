2085f38455b25de9009bce01af9d70611b6b4993
wei41 <wei41@avaya.com>
2019-01-21 12:10:10 +0800

    preloading

--- ng-stu/src/app/app.module.ts
+++ ng-stu/src/app/app.module.ts
@@ -1,9 +1,10 @@
 
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router'
+import { RouterModule, PreloadAllModules } from '@angular/router'
 import { AppComponent } from './app.component';
 import { appRoute } from './app-route';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyPreloading } from 'src/common/prelaoding-strategy';
 
 @NgModule({
   declarations: [
@@ -12,10 +13,11 @@ import { BrowserModule } from '@angular/platform-browser';
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoute,{
+      preloadingStrategy: MyPreloading,
       useHash: true
     })
   ],
-  providers: [],
+  providers: [MyPreloading],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
